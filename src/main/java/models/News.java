8292ebfda01f5be08ea5b0f05da3c0d78f775b75
kamotho-njiru <samuel.kamotho@student.moringaschool.com>
2021-09-27 16:13:05 +0300

    created constructors and methods to get info from the news category

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -4,4 +4,20 @@ public class News {
 
     // what is happening in various dpt
     // general information
+
+    private String generalinfo;
+    private String departmentinfo;
+
+    public News(){
+        this.generalinfo=generalinfo;
+        this.departmentinfo=departmentinfo;
+    }
+
+    public String getGeneralinfo() {
+        return generalinfo;
+    }
+
+    public String getDepartmentinfo() {
+        return departmentinfo;
+    }
 }
