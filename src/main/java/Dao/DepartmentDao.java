53ac20c428140de2591f7996ae7053af738c5404
kamotho-njiru <samuel.kamotho@student.moringaschool.com>
2021-09-28 04:58:16 +0300

    we added the CRUD method into our Dao

--- src/main/java/Dao/DepartmentDao.java
+++ src/main/java/Dao/DepartmentDao.java
@@ -1,6 +1,21 @@
 package Dao;
 
+import models.Department;
+import java.util.List;
+
 public interface DepartmentDao {
 
+//create
+    void add(Department department);
+    //void addDepartmentToNews
 
+    //read
+    List<Department> getAll();
+    // List<Department> getAllDepartments
+
+    //update
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
